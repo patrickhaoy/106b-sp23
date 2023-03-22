ff2522954e2a7db68413d7a97a6e8eb137c8cb5f
Class Account <ee106b-aba@c105-7.local>
2023-03-21 17:34:10 -0700

    dead reckoning done, other two questionable lol

---
+++ proj3/build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/cc/ee106b/sp23/class/ee106b-aba/Desktop/projects/proj3/src
+# Build directory: /home/cc/ee106b/sp23/class/ee106b-aba/Desktop/projects/proj3/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
+subdirs("proj3_pkg")
---
+++ proj3/build/proj3_pkg/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/cc/ee106b/sp23/class/ee106b-aba/Desktop/projects/proj3/src/proj3_pkg
+# Build directory: /home/cc/ee106b/sp23/class/ee106b-aba/Desktop/projects/proj3/build/proj3_pkg
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
